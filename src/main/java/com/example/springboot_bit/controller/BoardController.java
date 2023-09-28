87401acfb749b3c5211555fe8bbeb9d007ab4e60
saul <dlathf3210@naver.com>
2023-09-28 17:57:49 +0900

    MockMvc test

--- src/main/java/com/example/springboot_bit/controller/BoardController.java
+++ src/main/java/com/example/springboot_bit/controller/BoardController.java
@@ -1,7 +1,9 @@
 package com.example.springboot_bit.controller;
 
 import com.example.springboot_bit.service.BoardService;
+import com.example.springboot_bit.service.impl.BoardServiceImpl;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/board")
 @RequiredArgsConstructor
+@Slf4j
 public class BoardController {
 
     private final BoardService boardService;
     @GetMapping("/list")
     public String list(Model model){
         model.addAttribute("boardList", boardService.getList());
+        log.info("결과값 : {}", boardService.getList());
         return "list";
     }
 
--- src/test/java/com/example/springboot_bit/controller/BoardControllerTest.java
+++ src/test/java/com/example/springboot_bit/controller/BoardControllerTest.java
@@ -3,24 +3,41 @@ package com.example.springboot_bit.controller;
 import com.example.springboot_bit.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+
 @Slf4j
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 class BoardControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @Test
+    public void test() throws Exception {
+        log.info("--{}--",
+
+                mockMvc.perform(MockMvcRequestBuilders.get("/board/list"))
+                .andReturn()
+                .getModelAndView() );
+    }
 
 }
